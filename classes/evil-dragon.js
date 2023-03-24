70fbc2246d28e3295d767b17790edf90aa1e31ca
Britney <britney.rideaux@yahoo.com>
2023-03-23 23:34:40 -0500

    evildragons

--- classes/evil-dragon.js
+++ classes/evil-dragon.js
@@ -6,6 +6,44 @@ class EvilDragon extends Dragon {
     this.evilDoings = evilDoings;
     this.nemesis = nemesis;
   }
+  dontInviteThemOverForDinner() {
+    this.evilDoings.forEach((evilDoing) => {
+      console.log(`${this.name} will ${evilDoing}`);
+    });
+  }
+  burnsNemesis() {
+    return `${this.name} destroys ${this.nemesis} with fire! WHOOOSH!!!`;
+  }
 }
+
+const enderDragon = new EvilDragon(
+  "Ender Dragon",
+  "black",
+  [
+    "spit a fireball at Steve",
+    "rule over all the Endermen",
+    "destroy all blocks in its way",
+  ],
+  "Steve"
+);
+console.log(enderDragon);
+enderDragon.dontInviteThemOverForDinner();
+console.log(enderDragon.breathesFire());
+console.log(enderDragon.burnsNemesis());
+
+const drogon = new EvilDragon(
+  "Drogon",
+  "black-red",
+  [
+    "eat all your livestock",
+    "burn down your castle in Westeros",
+    "burn your father and brother",
+  ],
+  "Night King"
+);
+console.log(drogon);
+drogon.dontInviteThemOverForDinner();
+console.log(drogon.breathesFire());
+console.log(drogon.burnsNemesis());
 
 module.exports = EvilDragon;
