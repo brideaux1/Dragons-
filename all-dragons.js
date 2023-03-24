8e4f0fb41a47dc3f3925b4125e45ef5aef3efc83
Britney <britney.rideaux@yahoo.com>
2023-03-23 21:13:04 -0500

    Dragon class

--- all-dragons.js
+++ all-dragons.js
@@ -1,12 +1,16 @@
-// Your code here
-
+class Dragon {
+  constructor(name, color) {
+    this.name = name;
+    this.color = color;
+  }
+}
 
 /****************************************************************************/
 /******************* DO NOT EDIT CODE BELOW THIS LINE ***********************/
 
 try {
-  module.exports.falkor = falkor
-  module.exports.smaug = smaug
+  module.exports.falkor = falkor;
+  module.exports.smaug = smaug;
 } catch {
   module.exports = null;
 }
